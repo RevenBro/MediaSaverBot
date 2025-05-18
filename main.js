7d18fc10b19f8af46073d6b368e9ff6a3176fd60
RevenBro <saidakbarov077@gmail.com>
2025-05-18 14:55:08 +0500

    Removed token from code, added .env and .gitignore

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
-const TOKEN = "8078704952:AAF7GbNaHyQjBlgSClAbE2mCNVvYWo2z9jQ";
+import dotenv from "dotenv"
+dotenv.config()
+const TOKEN = process.env.BOT_TOKEN;
 const {instagramDownloaderMethod} = require("./instagram");
 
 const bot = new TelegramBot(TOKEN, {polling: true});
